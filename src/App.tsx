1ecfa863ff5e5748b2cb4849c9e17303ce70640a
SrJuggernaut <jugger@srjuggernaut.dev>
2023-07-23 22:31:07 -0600

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { type FC } from 'react'
+
+const App: FC = () => {
+  return (
+    <>
+      Hello World!
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import App from '@/App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(
+  document.getElementById('root') as Element)
+  .render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
