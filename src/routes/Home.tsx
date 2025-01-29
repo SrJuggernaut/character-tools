bf5ca65c1ca2ec20244d8ddddff4e17f527b66cd
SrJuggernaut <jugger@srjuggernaut.dev>
2025-01-29 16:06:05 -0600

    feat: add Manage Database to index page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -5,12 +5,40 @@ import { Card, CardActionArea, CardContent, IconButton, Typography } from '@mui/
 import { type FC } from 'react'
 import { Link } from 'react-router-dom'
 
+const cardsInfo = [
+  {
+    title: 'Character Editor',
+    description: 'Create and edit characters for AI Roleplay, with support for Image Cards, V1 and V2, and more!',
+    link: '/character-editor'
+  },
+  {
+    title: 'Character Library',
+    description: 'Manage your characters, make it easy to modify them, create copies and different versions',
+    link: '/character-library'
+  },
+  {
+    title: 'CharacterBook editor',
+    description: ' Edit or create characterBooks, a really useful tool to give context about the world or the character.',
+    link: '/characterbook-edito'
+  },
+  {
+    title: 'CharacterBook library',
+    description: 'Manage your characterBooks, make it easy to modify them, create copies and different versions',
+    link: '/characterbook-library'
+  },
+  {
+    title: 'Manage Database',
+    description: 'Manage your database, export, import and clear it',
+    link: '/manage-database'
+  }
+]
+
 const Home: FC = () => {
   return (
     <Fluid>
       <div
         css={{
-          minHeight: 'calc(100vh - 68px)',
+          minHeight: 'calc(100vh - 144px)',
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'center',
@@ -63,70 +91,25 @@ const Home: FC = () => {
             padding: '16px'
           }}
         >
-          <Card>
-            <CardActionArea
-              component={Link}
-              to="/character-editor"
-              sx={{
-                height: '100%'
-              }}
-            >
-              <CardContent>
-                <Typography variant="h3" align="center" gutterBottom>Character Editor</Typography>
-                <Typography variant="body1" align="center">
-                  Create and edit characters for AI Roleplay, with support for Image Cards, V1 and V2, and more!
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-          <Card>
-            <CardActionArea
-              component={Link}
-              to="/character-library"
-              sx={{
-                height: '100%'
-              }}
-            >
-              <CardContent>
-                <Typography variant="h3" align="center" gutterBottom>Character library</Typography>
-                <Typography variant="body1" align="center">
-                  Manage your characters, make it easy to modify them, create copies and different versions
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-          <Card>
-            <CardActionArea
-              component={Link}
-              to="/characterbook-editor"
-              sx={{
-                height: '100%'
-              }}
-            >
-              <CardContent>
-                <Typography variant="h3" align="center" gutterBottom>CharacterBook editor</Typography>
-                <Typography variant="body1" align="center">
-                  Edit or create characterBooks, a really useful tool to give context about the world or the character.
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-          <Card>
-            <CardActionArea
-              component={Link}
-              to="/characterbook-library"
-              sx={{
-                height: '100%'
-              }}
-            >
-              <CardContent>
-                <Typography variant="h3" align="center" gutterBottom>CharacterBook library</Typography>
-                <Typography variant="body1" align="center">
-                  Manage your characterBooks, make it easy to modify them, create copies and different versions
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
+          {cardsInfo.map((card) => (
+            <Card key={card.title}>
+              <CardActionArea
+                component={Link}
+                to={card.link}
+                sx={{
+                  height: '100%'
+                }}
+              >
+                <CardContent>
+                  <Typography variant="h3" align="center" gutterBottom>{card.title}</Typography>
+                  <Typography variant="body1" align="center">
+                    {card.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+
         </div>
       </div>
     </Fluid>
